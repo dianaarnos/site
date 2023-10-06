36a05f5ef9967e53d87cdae207a585254b624f57
Diana Ungaro Arnos <diana.arnos@gmail.com>
2023-10-06 21:34:54 +0200

    Commit inicial

---
+++ config/minicli.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    /****************************************************************************
+     * Minicli Settings
+     * You shouldn't need to change the next settings,
+     * but you are free to do so at your own risk.
+     *****************************************************************************/
+    'app_path' => [
+        __DIR__ . '/../app/Command',
+        '@librarianphp/command-help',
+        '@librarianphp/command-create',
+        '@librarianphp/command-cache',
+        '@librarianphp/command-web',
+        '@librarianphp/command-build',
+    ],
+    'theme' => 'unicorn',
+    'templates_path' => __DIR__ . '/../app/Resources/themes/default',
+    'data_path' => __DIR__ . '/../content',
+    'cache_path' => __DIR__ . '/../var/cache',
+    'stencil_dir' => __DIR__ . '/../app/Resources/stencil',
+    'stencil_locations' => [
+        'posts' => __DIR__ . '/../content/posts',
+        'pages' => __DIR__ . '/../content/pages',
+    ],
+    'rss_feed' => php_sapi_name() !== 'cli' ? 'feed' : 'feed.rss',
+];
---
+++ config_sample.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    /****************************************************************************************
+     * Librarian main config
+     * Values set here will overwrite default configuration from the /config dir.
+     *****************************************************************************************/
+
+    // Site Information
+    'site_name' => envconfig('SITE_NAME', 'Diana Arnos'),
+    'site_author' => envconfig('SITE_AUTHOR', '@dianaarnos'),
+    'site_description' => envconfig('SITE_DESC', 'Um espaço para centralizar as coisas que eu produzir e compartilhar.'),
+    'site_url' => envconfig('SITE_URL', 'http://localhost:8001'),
+    'site_root' => envconfig('SITE_ROOT', '/'),
+    'site_about' => envconfig('SITE_ABOUT', 'pages/sobre'),
+    // Set site_index if you want a custom index page
+    //'site_index' => 'getting-started/introduction',
+
+    'posts_per_page' => 10,
+
+    // Optional: Social links that show up on the top right
+    'social_links' => [
+        'Twitter' => envconfig('LINK_TWITTER', 'https://twitter.com/dianaarnos'),
+        'Github' => envconfig('LINK_GITHUB', 'https://github.com/dianaarnos'),
+        'YouTube' => envconfig('LINK_YOUTUBE'),
+        'LinkedIn' => envconfig('LINK_LINKEDIN', 'https://www.linkedin.com/in/dianaarnos/'),
+        'Twitch' => envconfig('LINK_TWITCH'),
+    ],
+];
